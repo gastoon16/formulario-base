fe42045f303d164be21f59eab953ecb74bf7475d
gaston <gtaborda@escuelasproa.edu.ar>
2022-03-09 12:18:34 -0300

    segundo push
    posible falla localhost

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("hola mounstruo");
+    console.log("Pagina de Inicio ");
+});
+
+app.post("/saludar", (req, res) => {
+    res.send("hola pana" + res.body.text + "" + res.body.text2);
 });
 
 app.listen(port, () => {
